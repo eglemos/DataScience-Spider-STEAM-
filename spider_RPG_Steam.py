52258e818f838cb8a0b86a3a6661f333607a1aba
eDuhz <dudugomeslemos@hotmail.com>
2017-11-13 08:58:07 -0200

    Itera sobre as páginas e descobre os links dos jogos, falta apenas entrar nas páginas dos jogos e pegar os dados. Obs.: existem algumas páginas de jogos que pedem algumas infos dos usuários, por exemplo, data de nascimento. Temos que ver como tratar isso, talvez seja impossiburu. Outras páginas apenas avisam que o conteúdo do jogo é violento e dá um warning, neste caso é só pegar o link do "continue" da página e já era.

--- spider_RPG_Steam.py
+++ spider_RPG_Steam.py
@@ -3,18 +3,16 @@
 # Created on 2017-11-09 11:12:50
 # Project: steam_project
 
-
-
 from pyspider.libs.base_handler import *
 import re
 
 class Handler(BaseHandler):
     crawl_config = {
     }
-    #starting URL: RPG section from steam site.
+    
     @every(minutes=24 * 60)
     def on_start(self):
-        self.crawl('http://store.steampowered.com/search/?tags=122', callback=self.index_page)
+        self.crawl('http://store.steampowered.com/search/?page=1', callback=self.index_page)
 
     @config(priority=2)
     def detail_page(self, response):
@@ -23,13 +21,14 @@ class Handler(BaseHandler):
             "title": response.doc('title').text(),
         }
 
-    #iterates over the RPG section from steam site.
+    #iterates over steam's main page (no tags)
+    #that will take a while...
     def index_page(self, response):
         for each in response.doc('a[href^="http"]').items():
-            if re.match("http://cdn.akamai.steamstatic.com/steam/apps/\d+/$", each.attr.href):
+            #for each game - collect data (TO DO)
+            if re.match("http://store.steampowered.com/app/\d+", each.attr.href):
                 self.crawl(each.attr.href, callback=self.detail_page)
-	#takes the last byte from the url string and adds 1. The result is the next page.        
-	index = int(response.url[-1]) + 1
+        #next index page
+        index = int(response.url[-1]) + 1
         response.url  = response.url[:-1] + str(index)
-	#recursive
         self.crawl(response.url, callback=self.index_page)
